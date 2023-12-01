ae15e2832d93e4f8f085d929b16f9af0b6ca4451
ItMan <itman7144@gmail.com>
2023-12-01 18:00:17 +0500

    upd

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__, template_folder='../docs', static_folder='../docs/static')
+# app = Flask(__name__, template_folder='../templates', static_folder='../templates/static')
+app = Flask(__name__, template_folder='templates', static_folder='templates/static')
+# app = Flask(__name__)
 app.config.from_object('config.Config')
 
 from .views import app
