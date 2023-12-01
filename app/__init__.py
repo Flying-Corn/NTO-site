2f8104253f8a16235f6ede0d9eace1f15e342b66
ItMan <itman7144@gmail.com>
2023-12-01 17:27:38 +0500

    deleting unnecessary files

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__, template_folder='../docs', static_folder='../docs/static')
-app.config.from_object('config.Config')
-
-from .views import app
--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-class Config:
-    pass
