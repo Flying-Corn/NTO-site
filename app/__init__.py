b825d29dceb98acc38a136ac1ecff3d0a9f76be1
ItMan <itman7144@gmail.com>
2023-12-01 18:28:45 +0500

    fixing the problem

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-# app = Flask(__name__, template_folder='../templates', static_folder='../templates/static')
-app = Flask(__name__, template_folder='templates', static_folder='templates/static')
-# app = Flask(__name__)
-app.config.from_object('config.Config')
-
-from .views import app
